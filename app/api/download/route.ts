0ba2a1bb36853f331f1533c46ce712fce3262ee6
holmirr707 <tohroku707@gmail.com>
2025-02-22 13:43:41 +0900

    ああ

--- app/api/download/route.ts
+++ app/api/download/route.ts
@@ -4,6 +4,7 @@ export const config = {
   runtime: 'edge',
 };
 
+
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
   const id = searchParams.get("id");
